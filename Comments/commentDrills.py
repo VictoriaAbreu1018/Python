f96a310348d3ea2840044635228ca1b8a4890208
VictoriaAbreu1018 <73201795+VictoriaAbreu1018@users.noreply.github.com>
2020-10-21 00:17:26 -0500

    drill changes

--- Comments/commentDrills.py
+++ Comments/commentDrills.py
@@ -17,20 +17,32 @@ START HERE
 
 #1) Make a single line comment.
 
-
+#Comments are my favorite part of coding
 
 #2) Make a multi-line comment.
 
-
+'''
+Hello
+My favorite dog breeds are:
+The Bernese Mountain dog
+Husky
+Goldens
+Mastiffs 
+'''
 
 #3) Make a variable. Then put a comment after it, on the same line.
 
-
+varB = 18 #I was born on the 18th of May
 
 #4) Make a single line comment.
 
-
+#When is ITA in person coming back?
 
 #5) Make a multi-line comment.
 
-
+'''
+I miss ITA in person
+I miss my cohort
+the food
+the instructors
+'''
