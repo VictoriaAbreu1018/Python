4de5f328c929dfc7c1d29f45b798685d768153e5
VictoriaAbreu1018 <73201795+VictoriaAbreu1018@users.noreply.github.com>
2020-10-21 00:11:16 -0500

    10-21 comment changes

---
+++ Comments/commentsOutline.py
@@ -0,0 +1,24 @@
+'''
+This outline will help solidify concepts from the Comments lesson.
+Fill in this outline as the instructor goes through the lesson.
+'''
+
+#1) Make a single line comment.
+
+#Hi my name is Victoria Abreu Socorro
+
+
+#2) Make a multi-line comment.
+
+'''
+Hi I'm Victoria
+I'm a junior
+And i'm in ITA 
+'''
+
+#3) Make an int variable. Then, make a single line comment on the 
+#same line, after the variable.
+
+var5 = 11 #my favorite superhero is spiderman 
+
+
