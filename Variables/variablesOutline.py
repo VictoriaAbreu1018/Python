42f02ed5187293e5c05e8bc3109c152a841617ca
VictoriaAbreu1018 <73201795+VictoriaAbreu1018@users.noreply.github.com>
2020-10-20 19:21:55 -0500

    Changes
    x

---
+++ Variables/variablesOutline.py
@@ -0,0 +1,88 @@
+'''
+This outline will help solidify concepts from the Variables lesson.
+Fill in this outline as the instructor goes through the lesson.
+'''
+
+#1)
+#Make an example variable of each of the types of variables (int, float, 
+#string, boolean and list):
+#Integer
+
+a = 5
+
+#Float 
+
+a = 3.56
+
+#string
+
+name = "Victoria"
+
+#boolean
+
+a = True 
+a = False 
+
+#list 
+
+a = [1,2,3,4]
+a = ["Cat","Dog","Fish" ]
+
+
+#2)
+#Make two variables one named 'a' and one named 'A' both set to  different data:
+#(Remember that capitalization matters, so these can be two different variables)
+#Integer
+
+a = 5
+
+#String
+
+A = "ITA"
+
+#3)
+#Make a variable, and then go down a couple lines and change the data in that
+#same variable:
+#Integer
+
+varB = 7
+
+
+varB = 11
+
+#4)
+#Make a variable and set it to a variable you have already created:
+#Integer
+
+varC = a
+
+#5)
+#Make a list with three items. Make a variable that is set to the second
+#item in that list:
+#list
+
+b = ["a", "b", "c"]
+
+listvar = b[1]
+
+
+#6)
+#Make a String set to your name. Make a variable that is set to the first item
+#in that list:
+#string
+
+string = "victoria"
+
+varD = string[0]
+
+print(varD)
+
+#7)
+#Print out ONE of any of the variables you have created so far:
+
+print(listvar)
+
+#8)
+#Make a variable of any type, with one of the common SYNTAX errors
+
+
