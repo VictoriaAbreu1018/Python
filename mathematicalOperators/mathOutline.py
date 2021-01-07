2ea9f6255df5e76e4f00a1a7f49d168be8b41cd2
VictoriaAbreu1018 <73201795+VictoriaAbreu1018@users.noreply.github.com>
2021-01-07 17:26:42 -0600

    math

---
+++ mathematicalOperators/mathOutline.py
@@ -0,0 +1,105 @@
+'''
+This outline will help solidify concepts from the Mathematical Operators lesson.
+Fill in this outline as the instructor goes through the lesson.
+'''
+
+#1) Make two string variables and use the + operator to combine them into
+#one new variable.
+var = "my"
+var1 = "name"
+
+x = var + var1
+print(x)
+#2) Make two int variables and use the + operator to combine them into
+#one new variable.
+
+run = 3
+run1 = 4
+
+y= run + run1
+print(y)
+
+#3) Make two int variables and use the - operator to combine them into
+#one new variable.
+
+v = 7
+v1 = 4
+
+z = v + v1
+print(z)
+#4) Make two float variables and use the - operator to combine them into
+#one new variable.
+
+c = 10.5
+c1 = 7.3
+
+f = c - c1
+print(f)
+
+#5) Make two int variables and use the * operator to combine them into
+#one new variable.
+
+int3 = 5
+int1 = 3
+
+h = int3 * int1
+print(h)
+
+#6) Make one string variable and one int variable and use the * operator 
+#to combine them into one new variable.
+
+s2= "victoria "
+s3= 6
+
+j= s2 * s3
+print(j)
+
+#7) Make two int variables and use the / operator to combine them into
+#one new variable.
+g1 = 10
+g2 = 5
+
+k= g1 / g2
+print(k)
+
+
+#8) Make two float variables and use the / operator to combine them into
+#one new variable.
+j1= 10.5
+j2= 2.5
+
+i= j1 / j2
+print(i)
+
+#9) Make two int variables and use the % operator to combine them into
+#one new variable.
+y3= 6
+f6= 3
+
+l= y3 % f6
+print(l)
+
+
+#10) Make two int variables and use the // operator to combine them into
+#one new variable.
+log = 8
+log2 = 4
+
+o = log // log2 
+print(o)
+
+
+#11) Make two int variables and use the ** operator to combine them into
+#one new variable.
+e = 5
+e4 = 3
+
+t= e ** e4
+print(t)
+
+
+#12) Make any type of operation with a common SYNTAX error
+
+string = "victoria "
+
+print("this is"  string)
