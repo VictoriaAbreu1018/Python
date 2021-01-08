8fa76be8bd31fa513971e7ca5ed851e2e3fd6530
VictoriaAbreu1018 <73201795+VictoriaAbreu1018@users.noreply.github.com>
2021-01-08 17:15:13 -0600

    =

---
+++ ComparisonOperators/comparisonOutline.py
@@ -0,0 +1,55 @@
+'''
+This outline will help solidify concepts from the Comparison Operators lesson.
+Fill in this outline as the instructor goes through the lesson.
+'''
+from _socket import AF_APPLETALK
+
+#1) Make two string variables. Compare them using the == operator and store
+#that comparison in a new variable. Then print the variable.
+d = "apple"
+f = "apple" 
+v = (d == f)
+print(v)
+
+
+#2) Make two int variables. Compare them using the == operator and store
+#that comparison in a new variable. Then print the variable.
+g = 4
+h = 4
+j = (g == h)
+print(j)
+
+#3) Make two int variables. Compare them using the > operator and store
+#that comparison in a new variable. Then print the variable.
+y = 3
+c = 5
+l = (c > y)
+print(l)
+
+
+#4) Make two int variables. Compare them using the < operator and store
+#that comparison in a new variable. Then print the variable.
+two = 2
+four = 4
+o = (two < four)
+print(o)
+
+#5) Make two int variables. Compare them using the >= operator and store
+#that comparison in a new variable. Then print the variable.
+red = 2
+blue = 1
+
+r = (red >= blue)
+print(r)
+
+
+#6) Make two int variables. Compare them using the <= operator and store
+#that comparison in a new variable. Then print the variable.
+green = 3
+purple = 6
+u = (purple <= green)
+print(u)
+
+#7) Make a comparison with any operator, with one of the common SYNTAX errors.
+
+
