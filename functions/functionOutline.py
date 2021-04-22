a299928312df57a1197964462119153f4c1986b1
VictoriaAbreu1018 <73201795+VictoriaAbreu1018@users.noreply.github.com>
2021-04-22 13:59:28 -0500

    functionsoutline

--- functions/functionOutline.py
+++ functions/functionOutline.py
@@ -7,17 +7,31 @@ Fill in this outline as the instructor goes through the lesson.
 #true, return true. Else, return false. Then, call the function.
 #Print what the function returns.
 
-
-
+def booleanvars (booone , bootwo):
+    if(booone == bootwo):
+        return "True"
+    else:
+        return "False"
+lean = booleanvars(2, 3)
+print(lean)
 #2) Make a function that takes one int parameter: gallons. Convert gallons
 #to cups (do this by multiplying gallons by 16). Then return cups. Then,
 #call the function.
 #Print what the function returns.
+def galsandcups (gallons):
+    cups = gallons * 16
+    return cups
 
+galls = galsandcups(5)
+print(galls)
 
 
 #3) Make a function of any any kind with a common SYNTAX error. Then, call the
 #function.
 #Print what the function returns.
+def oneandtwo ony,twy):
+    sum = ony + twy
+    return sum
 
-
+num = oneandtwo(1, 2)
+print(num)
