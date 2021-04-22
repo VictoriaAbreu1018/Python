31441db34edb764923ca46e4698f7789631dfd3f
VictoriaAbreu1018 <73201795+VictoriaAbreu1018@users.noreply.github.com>
2021-04-22 13:10:10 -0500

    loopsdrills

--- looops/loopsDrills.py
+++ looops/loopsDrills.py
@@ -25,32 +25,51 @@ START HERE
 #    you just created and decrements the variable by one each time through
 #    the loop. Meanwhile, make the loop run until the variable you created 
 #    equals 1.
-
+x = 4
+while(x >= 1):
+    print(x)
+    x -= 1
 
 
 #2) Declare a variable set to 14. Make a while loop that prints the variable 
 #    you just created and increments the variable by one each time through
 #    the loop. Meanwhile, make the loop run until the variable you created 
 #    equals 20.
-
+c = 14
+while(c <= 20):
+    print(c)
+    c = c + 1
 #3) Declare a variable set to 55. Make a while loop that prints the variable 
 #   you just created. Then make an if statement that makes the loop break when
 #   the variable is equal to 50. 
-
+g = 55
+while (g >= 40):
+    print(g)
+    g -= 1
+    if(g == 50):
+        break
+    
 
 '''For Loops'''
 #4) Create a list named sports. Put three sports into the list. Create
 #   a for loop that prints each sport in the list
-
+sports = ["baseball", "volleyball", "basketball"]
+for x in sports:
+    print(x)
 #5) Create a for loop that loops through each letter in a string of one of your
 #   favorite songs. Each iteration should print should a letter of the word. 
-
+a = "Train Wreck"
+for x in a:
+    print(x)
 #6) Create a list named movies. Put five of your favorite movies into the list.
 #   However, make sure one of the movies is Avatar. 
 #   Create a for loop that iterates over the list. In the loop print the movie
 #   being looped over, but create an if statement that breaks out of the 
 #   loop if it is Avatar.
-
-
+movies = ["London has fallen", "Olympus has fallen", "Conjuring", "Avatar", "Aristocats"]
+for c in movies:
+    print(c)
+    if( c == "Avatar"):
+        break
 
 
